ccf30de51840bc6bd87304a3e2b431fc8f0abf56
tomfatoke <tomfatoke@gmail.com>
2026-04-26 22:57:00 +0000

    Created silver and bronze layers of medallion architecture.

--- Notebooks/01_bronze_ingestion.py
+++ Notebooks/01_bronze_ingestion.py
@@ -1,1 +1,18 @@
+from pyspark.sql.functions import current_timestamp, lit
 
+
+file_path = "/Volumes/projects/rent_project/rent_raw_data/46100092.csv" 
+rent_df = spark.read \
+.option("header", "true") \
+.option("inferSchema", "true") \
+.csv("/Volumes/projects/rent_project/rent_raw_data/46100092.csv")    .withColumnRenamed("Rental unit type", "Rental_unit_type")
+
+
+rent_df.printSchema() 
+rent_df.show(10) 
+
+rent_df.select("VALUE", "UOM", "Estimates", "Rental_unit_type", "GEO", "REF_DATE").show(20, truncate = False) 
+
+rent_df.write.format("delta").mode("overwrite").saveAsTable("projects.rent_project.bronze_rent_raw") 
+
+print(f"Total rown written: {rent_df.count()}")
--- Notebooks/02_silver_cleaning.py
+++ Notebooks/02_silver_cleaning.py
@@ -1,1 +1,38 @@
+from pyspark.sql.functions import col, split, when, regexp_replace, trim 
 
+silver_df = spark.read.format("delta").table("projects.rent_project.bronze_rent_raw") 
+
+silver_df.show(5)
+silver_df = silver_df.select("REF_DATE", "GEO", "Rental_unit_type", "VALUE") 
+silver_df = silver_df.dropna(subset=["VALUE"]) 
+
+print(f"rows remaining after cleaning: {silver_df.count()}") 
+silver_df.show(5) 
+
+silver_df = silver_df.withColumn("City", 
+                                 trim(split(col("GEO"), ",")[0])) \
+                                     .drop("GEO") 
+silver_df.show(5) 
+
+silver_df = (silver_df 
+    .withColumn("Unit_type", 
+                split(col("Rental_unit_type"), " - ")[0])  
+    .withColumn("Bedrooms", 
+                when(col("Rental_unit_type") == "Room", "N/A") 
+                .otherwise(split(col("Rental_unit_type"), " - ")[1])) 
+    .drop("Rental_unit_type")  
+)
+silver_df.show(10) 
+
+silver_df = (silver_df 
+    .withColumnRenamed("REF_DATE", "Reference_Date") 
+    .withColumnRenamed("VALUE", "Asking_rent") 
+    .withColumn("Asking_rent", 
+                col("Asking_rent").cast("float"))
+) 
+
+silver_df.show(5) 
+
+silver_df.write.format("delta").mode("overwrite").saveAsTable("projects.rent_project.silver_rent_layer")
+
+print(f"Rows written to silver: {silver_df.count()}")
